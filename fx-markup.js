1a29a5a445660a65dfc0cc34be75325116b1d0c1
pkellogg <pat@kztek.com>
2020-08-03 07:30:33 -0500

    Add files via upload
    This is the original markup component as a shadow dom

--- fx-markup.js
+++ fx-markup.js
@@ -2,6 +2,20 @@
 	
 	const template = document.createElement('template');
 	template.innerHTML = `
+	
+	<style>
+	.fxmarkupclass {
+		border:none;
+		background-color: tan;
+		position:relative;
+		text-align:left;
+		overflow-wrap:break-word;
+		 
+	}
+	</style>
+	
+	 
+	
     <div class="fxmarkupclass">
 	<slot name="element-name" ></slot>
 	</div>`;
@@ -39,11 +53,47 @@
 						  ' font-size: ' + elem.getAttribute('size') + 'px;' +  
 						  ' color: ' + elem.getAttribute('textcolor') + ';' + 
 						  ' font-family: ' + elem.getAttribute('family') + ';' + '}'  
-						  
-						  
 	}
   }
 } 
 	
 	window.customElements.define('fx-markup', FxMarkup);
 })();
+
+
+//custom element only version to be host of other elements with that have a shadow root...so as to not duplicate shadow roots
+/*function() {
+	
+	const template = document.createElement('template');
+	template.innerHTML = `
+	
+	<style>
+	.fxmarkupclass {
+		border:none;
+		background-color: tan;
+		position:relative;
+		text-align:left;
+		overflow-wrap:break-word;
+		 
+	}
+	</style>
+	
+	 
+	
+    <div class="fxmarkupclass">
+	<slot name="element-name" ></slot>
+	</div>`;
+
+	class FxMarkup extends HTMLElement {
+	 
+	 
+	connectedCallback() {
+	const node = document.importNode(template.content, true);
+    this.appendChild(node);
+	}   
+	
+	 
+} 
+	
+	window.customElements.define('fx-markup', FxMarkup);
+})();*/
